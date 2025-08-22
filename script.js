1fad3be193c645ae0d881c171d2f68659f19ce99
Shrey Kansara <shreyhiralkansara@gmail.com>
2025-08-22 13:45:51 +0530

    Box color changes added

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 let btn = document.getElementById('greet').addEventListener('click', () => {
-    let name = document.getElementById('input').value
-    console.log(name);
+    // document.getElementById('name').innerText = 'Hello, ' + document.getElementById('input').value
+    document.getElementById('name').innerText = `Hello, + ${document.getElementById('input').value}`
 })
+
+let red = document.getElementById('red')
+red.addEventListener('click', () => {
+    red.style.backgroundColor = 'red';
+    red.style.color = 'white'
+})
+
+let blue = document.getElementById('blue')
+blue.addEventListener('click', () => {
+    blue.style.backgroundColor = 'blue';
+    blue.style.color = 'white'
+})
+
+let green = document.getElementById('green')
+green.addEventListener('click', () => {
+    green.style.backgroundColor = 'green';
+    green.style.color = 'white'
+})
+
+let yellow = document.getElementById('yellow')
+yellow.addEventListener('click', () => {
+    yellow.style.backgroundColor = 'yellow';
+    yellow.style.color = 'white'
+})
+
 
